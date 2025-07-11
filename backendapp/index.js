88b1fa9e0cece359db51af9b83ef6d01f1c5e261
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-07-12 02:25:45 +0530

    ..

--- backendapp/index.js
+++ backendapp/index.js
@@ -1,3 +1,13 @@
 const express=require("express");
 const app=express();
 const port =3000;
+
+
+app.get('/',(req,res)=>{
+    res.send("Shahid");
+})
+
+
+app.listen(port,()=>{
+    console.log(`Listening at port ${port}`);
+})
