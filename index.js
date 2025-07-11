e1c658b618729346ef624b25402f4996b4dd36ab
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-07-12 00:35:26 +0530

    Sending file in get request

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ const express=require("express");
 const app=express();
 const port =3000;
 
+// app.get('/',(req,res)=>{
+//     res.send('Hello World');
+// })
+
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.sendFile('D:/Node/dummy.html');
 })
+
+app.get('/about',(req,res)=>{
+    res.send("Got an about");
+})
+
+
+app.post('/about',(req,res)=>{
+    res.send("Got post about");
+})
+
+app.delete('/about',(req,res)=>{
+    res.send("Got delete about");
+})
+app.put('/about',(req,res)=>{
+    res.send("Got put about");
+})
+
 
 app.listen(port,()=>{
     console.log(`Listening to port ${port}`);
