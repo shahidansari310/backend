fcb2fd5ea0793fa996f447dea79a8c152581e0f7
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-07-12 13:34:50 +0530

    custom middleware

--- backendapp/index.js
+++ backendapp/index.js
@@ -2,8 +2,31 @@ const express=require("express");
 const app=express();
 const port =3000;
 
+app.use(express.json()); //inbuilt middleware
+
+//Custom middleware
+//logging middleware
+
+const logging=function(req,res,next){
+    console.log("Logging IN");
+    next();
+}
+app.use(logging);
+
+const auth=function(req,res,next){
+    console.log("Auth");
+    next();
+}
+app.use(auth);
+
+const valid=function(req,res,next){
+    console.log("Valid");
+    next();
+}
+app.use(valid);
 
 app.get('/',(req,res)=>{
+    console.log(req.body);
     res.send("Shahid");
 })
 
