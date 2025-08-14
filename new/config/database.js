b8e4cd6f2b8a7840c1d9fc317627bf87988dfaed
shahidansari310 <shahidansaribhodu@gmail.com>
2025-08-14 22:55:59 +0530

    backend 2

---
+++ new/config/database.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+
+require("dotenv").config();
+
+const dbConnect=()=>{
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(()=>console.log("Db connected successfully"))
+    .catch((err)=> {
+        console.log("error in connection");
+        console.error(err.message);
+        process.exit(1);
+    })
+}
+
+module.exports= dbConnect;
---
+++ new/models/Todo.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+
+const todoschema=new mongoose.Schema(
+    {
+        title:{
+            type:String,
+            required:true,
+            maxlength:50
+        },
+        description:{
+            type:String,
+            required:true,
+            maxlength:50
+        },
+        createdAt:{
+            type:Date,
+            required:true,
+            default:Date.now(),
+        },
+        UpdatedAt:{
+            type:Date,
+            required:true,
+            default:Date.now(),
+        }
+    }
+)
+
+
+
+module.exports=mongoose.model("Todo",todoschema);
