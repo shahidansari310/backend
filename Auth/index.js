78f56d8d2956f6f88ba4119373d8d725eb23a046
shahidansari310 <shahidansaribhodu@gmail.com>
2025-09-27 01:30:25 +0530

    html file send

--- Auth/index.js
+++ Auth/index.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const app=express() 
+const path = require("path"); 
 const PORT=4000
 const birds=require("./routes/birds")
 
@@ -8,7 +9,8 @@ app.use(express.json())
 app.use("/v1",birds);
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    // res.send("Hello World");
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
 app.listen(PORT,()=>{
