f64ab388a729e8b6aaf3ab758ff9cf1820766751
shahidansari310 <shahidansaribhodu@gmail.com>
2025-12-01 02:45:34 +0530

    edit

--- sheriyans/crudejs/index.js
+++ sheriyans/crudejs/index.js
@@ -38,6 +38,20 @@ app.get('/delete/:id',async (req,res)=>{
     res.redirect("/read");
 })
 
+app.get('/edit/:id',async (req,res)=>{
+    let user=await userModel.findOne({_id:req.params.id});
+    res.render("edit",{user});
+    
+})
+
+app.post('/update/:id',async (req,res)=>{
+    let {name,email,image}=req.body;
+    let user=await userModel.findOneAndUpdate({_id:req.params.id},{name,email,image},{new:true});
+    // res.render("edit",{user});
+    res.redirect("/read");
+    
+})
+
 app.listen(PORT,()=>{
     console.log(`App running on port :${PORT}`);
 })
